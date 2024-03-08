5cefe0da9333adc7f09f9caf719973e00a1f28bb
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2024-03-08 11:14:23 -0300

    repo

---
+++ src/main/java/com/test/primary_project/PrimaryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.primary_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/primary_project/PrimaryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.primary_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
